2e585dda9093a820bbfcea9b191f9d5e21f046f9
ubhosle <ubhosle@ncsu.edu>
2017-10-17 00:38:37 -0400

    Git Integration

---
+++ downloadFromGit.js
@@ -0,0 +1,65 @@
+var Botkit = require('botkit');
+var request = require('request');
+var fs = require("fs");
+var wget = require('node-wget');
+function downloadFile(url) {
+    //console.log("in the dowload file function");
+    try
+    {
+        var fileName = wget(url);
+        console.log(fileName);
+    }
+    catch(Exception)
+    {
+        console.log("error in download file function");
+    }
+}
+
+function getFileType(url) {
+    var fileType = "";
+    //check if the url ends with a token or not
+    var substring = "?token";
+    if (url.indexOf(substring) != -1)
+    {
+        fileType = url.substring(url.lastIndexOf('.')+1,url.indexOf('?'));
+    }
+    else
+    {
+        fileType = url.substring(url.lastIndexOf('.')+1, url.length);
+    }
+    console.log("Your file type is: " + fileType);
+    return fileType;
+}
+var childProcess = require("child_process");
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: process.env.SLACKTOKEN,
+}).startRTM()
+
+// give the bot something to listen for.
+//controller.hears('string or regex',['direct_message','direct_mention','mention'],function(bot,message) {
+controller.hears('raw.github', ['mention', 'direct_mention', 'direct_message'], function (bot, message)
+{
+    console.log("printing message after this"); 
+    console.log(message);
+    console.log("done printing message");
+    var url = message.text;
+    var urlLength = url.length;
+    url = url.substring(1, urlLength - 1);
+    console.log(url);
+    downloadFile(url);
+    bot.reply(message, "Processing your code :)");
+    var fileType = getFileType(url);
+    bot.reply(message, "The filetype you uploaded is: " + fileType);
+    // process the file in the api. get data
+    bot.reply(message, "your file was processed.\nHere are some points...");
+  }
+);
+
