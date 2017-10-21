89c94bc08d7b4b4042182c9e892dea448d517e97
Rohil Shah <rshah8@ncsu.edu>
2017-10-21 11:48:12 -0400

    handling input errors

--- bot.js
+++ bot.js
@@ -32,6 +32,12 @@ controller.hears('hi','direct_mention,direct_message', function(bot, message) {
       if(type.includes("github")){
         convo.ask('Please provide the link to the raw file.', function(answer1, convo){
           var gitLink=answer1.text;
+          if(gitLink.includes("goodbye")|| gitLink.includes("bye"))
+          {
+            convo.next();
+            convo.say("Good Bye!");
+              return;
+          }
           gitLink = gitLink.substring(1, (gitLink.length - 1));
           convo.next();
           convo.say('great');
@@ -50,25 +56,39 @@ controller.hears('hi','direct_mention,direct_message', function(bot, message) {
       {
         convo.ask('Please upload the code file', function(answer2, convo){
           console.log(answer2);
+          if(typeof answer2.file=='undefined')
+          {
+            //var private=answer2.file.url_private_download;
+
+            convo.next();
+            convo.say("Sorry I dont follow, exiting, try again from the start");
+            return;
+          }
           var private=answer2.file.url_private_download;
           var slug = private.split('.com').pop();
           console.log(slug);
 
           var permalink=answer2.file.permalink;
           var options = {
-        "method": "GET",
-        "hostname": "files.slack.com",
-        "path": slug,
-        "rejectUnauthorized": "true",
-        "headers": {
-            "Authorization": "Bearer xoxp-256865299430-256034721060-256170554661-e9e93acfc3251d0d547cc9ca00ef1a38"
-        }
-      }
-      downloader.pDownload(slug,permalink,"C:/Users/rgsha/Documents/Projects/SE/SlackBot/test.js");
-      convo.next();
-      convo.say("Please Wait, analyzing");
-      
+            "method": "GET",
+            "hostname": "files.slack.com",
+            "path": slug,
+            "rejectUnauthorized": "true",
+            "headers": {
+                "Authorization": "Bearer xoxp-256865299430-256034721060-256170554661-e9e93acfc3251d0d547cc9ca00ef1a38"
+            }
+          }
+          downloader.pDownload(slug,permalink,"C:/Users/rgsha/Documents/Projects/SE/SlackBot/test.js");
+          convo.next();
+          convo.say("Please Wait, analyzing");
+          
         });
+      }
+      else if(type.includes("goodbye")|| type.includes("bye"))
+      {
+        convo.next();
+        convo.say("Good Bye!");
+        return;
       }
       else
       {
