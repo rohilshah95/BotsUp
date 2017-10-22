1cf9d0371d32f6113bd3b9d2e73474adf7b7d334
Vijay Hebbar <vhhebbar@ncsu.edu>
2017-10-22 18:52:45 -0400

    Method parser imrovements.

--- parser_files/parse.js
+++ parser_files/parse.js
@@ -1,19 +1,62 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
-const docFile = "D:/CSC510/bot/sample.html";
+const docFile = "D:/CSC510/bot/java_string.html";
+var $ 
+
+var methodQuery = {
+	method_name : "hashCode"
+}
+
+var classQuery = {
+	method_name : "String"
+}
+ var classTest = $('a[name=constructor_summary]').next().next().
+find("code:contains('String')").parent().parent().text();
+
+var methodTest = $('a[name=method_summary]').next().next().
+find("code:contains('hashCode')").parent().parent().text();
 
 fs.readFile(docFile, 'utf8', function(err,contents){
-	var $ = cheerio.load(contents);
-//gets the id of all apple classes
-//console.log($('.apple').attr('id'));
-//get the text of all the items with class = chips and  within id 'snacks'
-//console.log($('.chips', '#snacks').text());
+	 $ = cheerio.load(contents);
 
-//console.log($('#fruits').contents().attr('id'));
-//console.log($($('#fruits').find($('.apple')).length));
-//get all items 
-//console.log($('#one').find($('.apple')).attr('taste'));
-//console.log($('#fruits').nextUntil($('#sweets')).attr('id'));
-//console.log($('#one'));
-console.log($('#fruits').children().has('funny').attr('id'));
-});
+console.log(getMethodDetails(methodQuery.method_name));
+});  
+
+function getMethodDetails(methodName){
+	var array = [];
+	var result = $('a[name=method_summary]').next().next().
+					find("code:contains(" + methodName + ")").parent().parent().map(function(){
+		
+						var eachText = $(this).text().trim().split(")");
+						var section = eachText[0].split('\n');
+						var index =1;
+						var method_name="";
+						while (index < section.length ){
+							method_name +=section[index].trim();
+							index ++;
+						}
+						array.push({
+							"method_name" : method_name.trim() + ")",
+							"return_type" : section[0].trim(),
+							"description" : eachText[1].replace(/\n|\r/g,'').replace(/\s+/g, " ").trim()
+						
+					});
+				});
+	return array;
+	// if array is empty handle exception.
+
+}
+function getClassDetails(className){
+	var array = [];
+	var result = $('a[name=constructor_summary]').next().next().
+					find("code:contains(" + className + ")").parent().parent().map(function(){
+						var eachText = $(this).text().split("\n");
+						var eachJson = {
+							"constructor_name" : eachText[0].trim(),
+							"description" : eachText[1].trim()
+						}
+						array.push(eachJson);
+					});
+	return array;
+	// if array is empty handle exception.
+}
