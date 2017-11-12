f266dd1301451186661a63ffd15b4390db501f09
Vijay Hebbar <vhhebbar@ncsu.edu>
2017-11-12 01:44:32 -0500

    beautified some code

--- sonar.js
+++ sonar.js
@@ -13,7 +13,7 @@ const urlRoot = "http://localhost:9000";
 var analyse = function (scanOptions) {
   return new Promise(function (resolve, reject) {
     child_process.exec("sonar-scanner " + makeParams(scanOptions), function (error, stdout, stderr) {
-      sleep.sleep(5); //sleeping for 2 seconds to check if webserver responds
+     // sleep.sleep(5); //sleeping for 2 seconds to check if webserver responds
       console.log("Scanner resolved with id " + scanOptions.session_id)
       resolve(scanOptions.session_id);
       if(!error && !stderr){
@@ -25,14 +25,9 @@ var analyse = function (scanOptions) {
   });
 }
 
-var getIssues = function(sessionID) {
-  return request.get(`${urlRoot}/api/issues/search?componentKeys=${sessionID}`)
+var getIssues = (sessionID) => request.get(`${urlRoot}/api/issues/search?componentKeys=${sessionID}`);
+var getRules = (rule) =>  request.get(`${urlRoot}/api/rules/show?key=${rule}`)
 
-};
-
-var getRules = function(rule) {
-   return request.get(`${urlRoot}/api/rules/show?key=${rule}`)
-};
 
 function makeParams(scanOptions) {
   var params = "-Dsonar.projectBaseDir=" + scanOptions.directory + " -Dsonar.projectKey=" + scanOptions.session_id + " -Dsonar.projectName=" + scanOptions.session_id + " -Dsonar.sources=.";
