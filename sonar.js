b5b788bd8cb2474865da056a8847d83211c8b48f
Vijay Hebbar <vhhebbar@ncsu.edu>
2017-11-12 13:02:06 -0500

    Implemented logic to retrieve issues at the right time
    Implemented a retry callback for superagent. This takes in a condition that is evaluated on every retry - Retry until the status of the scan of the current session id is 'success' or 'failed'.
    
    We no longer need to sleep for a fixed duration before fetching issues. The request to sonarqube web api handles this.
    
    Issue #8 fixed

--- sonar.js
+++ sonar.js
@@ -1,33 +1,54 @@
 const child_process = require('child_process');
 const request = require('superagent');
-//const nock = require('nock');
 const sleep = require('sleep');
-//var mockData = require("./mockData.json")
-//const username = "admin";
-//var password = "admin";
-//var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-// Use server IP instead of localhost if Sonarqube is not deploy on this machine.
 const urlRoot = "http://localhost:9000";
-
 
 var analyse = function (scanOptions) {
   return new Promise(function (resolve, reject) {
     child_process.exec("sonar-scanner " + makeParams(scanOptions), function (error, stdout, stderr) {
-      sleep.sleep(5); //sleeping for 2 seconds to check if webserver responds
       console.log("Scanner resolved with id " + scanOptions.session_id)
       resolve(scanOptions.session_id);
-      if(!error && !stderr){
+      if (!error && !stderr) {
       }
       else {
-      //  reject(stderr);
+        //  reject(stderr);
       }
     });
   });
 }
 
-var getIssues = (sessionID) => request.get(`${urlRoot}/api/issues/search?componentKeys=${sessionID}`);
-var getRules = (rule) =>  request.get(`${urlRoot}/api/rules/show?key=${rule}`)
+var getIssues = function(sessionID) {
+    return request.get(`${urlRoot}/api/ce/component?componentKey=${sessionID}`).retry(1000000, retryCondition )
+            .then(function(){
+              console.log(sessionID);
+              return request.get(`${urlRoot}/api/issues/search?componentKeys=${sessionID}`)
+            })
+}
 
+var retryCondition = (err, res) => {
+  // if(("object" == typeof res) && res.body && res.body.current){
+  //   console.log(res.body.current.status)
+  // }
+  // if(("object" == typeof res) && res.body && res.body.queue){
+  //   console.log(res.body.queue[0].status)
+  // }
+  return !(("object" == typeof res) && res.body && res.body.current && (res.body.current.status === "SUCCESS" || res.body.current.status === "FAILED"))
+
+}
+
+var getRules = function(rule) {
+  return new Promise(function(resolve,reject){
+    request
+    .get(`${urlRoot}/api/rules/show?key=${rule}`)
+    .end(function(err, res) {
+      if (!err) {
+        resolve(res);
+      } else {
+        reject(err);
+      }
+    });
+  })
+}
 
 function makeParams(scanOptions) {
   var params = "-Dsonar.projectBaseDir=" + scanOptions.directory + " -Dsonar.projectKey=" + scanOptions.session_id + " -Dsonar.projectName=" + scanOptions.session_id + " -Dsonar.sources=.";
@@ -38,6 +59,14 @@ module.exports.analyse = analyse;
 module.exports.getIssues = getIssues;
 module.exports.getRules = getRules;
 
+
+//const nock = require('nock');
+
+//var mockData = require("./mockData.json")
+//const username = "admin";
+//var password = "admin";
+//var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+// Use server IP instead of localhost if Sonarqube is not deploy on this machine.
 // Mock the request using Nock
 // Returns issues array
 // nock('http://localhost:9000')
